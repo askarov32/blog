2177961dfb54516ae28c0925fd427fb46bf860a1
Insar Askarov <insar@lessons.kz>
2024-06-19 23:42:28 +0500

    Added Navigation to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,25 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    You are logged in!
+
+                    <div class="mt-4">
+                        <h4>User Information</h4>
+                        <ul>
+                            <li><strong>Name:</strong> {{ Auth::user()->name }}</li>
+                            <li><strong>Email:</strong> {{ Auth::user()->email }}</li>
+                        </ul>
+                    </div>
+
+                    <div class="mt-4">
+                        <h4>Navigation</h4>
+                        <ul>
+                            <li><a href="{{ route('posts.index') }}">View Posts</a></li>
+                            <li><a href="{{ route('posts.create') }}">Create Post</a></li>
+                            <li><a href="{{ route('categories.index') }}">View Categories</a></li>
+                            <li><a href="{{ route('roles.index') }}">View Roles</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
