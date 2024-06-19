64a5df6cc7a310a75a80e381c9d8628df00d7452
Insar Askarov <insar@lessons.kz>
2024-06-20 01:22:01 +0500

    Modified AdminMiddleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -3,19 +3,20 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        if (auth()->user() && auth()->user()->hasRole('admin')) {
+        if (Auth::check() && Auth::user()->hasRole('admin')) {
             return $next($request);
         }
 
