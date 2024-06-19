0ec6bbb3e40d994a2c7d55ce86cf499f26e126ab
Insar Askarov <insar@lessons.kz>
2024-06-20 03:07:29 +0500

    Created policy for posts

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user, $ability)
+    {
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
+    public function viewAny(User $user)
+    {
+        return $user->hasAnyRole(['admin', 'moderator']);
+    }
+
+    public function view(User $user, Post $post)
+    {
+        return $user->hasAnyRole(['admin', 'moderator']);
+    }
+
+    public function create(User $user)
+    {
+        return $user->hasRole('admin');
+    }
+
+    public function update(User $user, Post $post)
+    {
+        return $user->hasAnyRole(['admin', 'moderator']);
+    }
+
+    public function delete(User $user, Post $post)
+    {
+        return $user->hasRole('admin');
+    }
+}
