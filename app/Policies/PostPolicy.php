554dc6f775a77b39688dcf305e13cbd1d6c283b5
Insar Askarov <insar@lessons.kz>
2024-06-20 15:12:55 +0500

    Updated Post Policy

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -2,42 +2,66 @@
 
 namespace App\Policies;
 
-use App\Models\Post;
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PostPolicy
 {
     use HandlesAuthorization;
 
-    public function before(User $user, $ability)
-    {
-        if ($user->hasRole('admin')) {
-            return true;
-        }
-    }
-
+    /**
+     * Determine whether the user can view any posts.
+     */
     public function viewAny(User $user)
     {
-        return $user->hasAnyRole(['admin', 'moderator']);
+        return $user->hasRole('admin') || $user->hasRole('moderator');
     }
 
+    /**
+     * Determine whether the user can view the post.
+     */
     public function view(User $user, Post $post)
     {
-        return $user->hasAnyRole(['admin', 'moderator']);
+        return $user->hasRole('admin') || $user->hasRole('moderator');
     }
 
+    /**
+     * Determine whether the user can create posts.
+     */
     public function create(User $user)
     {
         return $user->hasRole('admin');
     }
 
+    /**
+     * Determine whether the user can update the post.
+     */
     public function update(User $user, Post $post)
     {
-        return $user->hasAnyRole(['moderator']);
+        return $user->hasRole('moderator');
     }
 
+    /**
+     * Determine whether the user can delete the post.
+     */
     public function delete(User $user, Post $post)
+    {
+        return $user->hasRole('admin');
+    }
+
+    /**
+     * Determine whether the user can restore the post.
+     */
+    public function restore(User $user, Post $post)
+    {
+        return $user->hasRole('admin');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the post.
+     */
+    public function forceDelete(User $user, Post $post)
     {
         return $user->hasRole('admin');
     }
